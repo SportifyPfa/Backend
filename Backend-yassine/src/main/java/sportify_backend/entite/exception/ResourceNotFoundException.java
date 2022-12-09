70ea90b7c982884786169c67818908036971313e
oumaima136 <oumaimabenatia24@gmail.com>
2022-12-09 14:09:39 +0100

    render

---
+++ Backend-yassine/src/main/java/sportify_backend/entite/exception/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package sportify_backend.entite.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException() {
+        super();
+    }
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    public ResourceNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
