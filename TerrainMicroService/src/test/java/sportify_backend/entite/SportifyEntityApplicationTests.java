159db39f7c686fa6c3c6e124e1c3c901f558106e
oumaima136 <46504468+oumaima136@users.noreply.github.com>
2022-12-10 00:19:43 +0100

    terrain

---
+++ TerrainMicroService/src/test/java/sportify_backend/entite/SportifyEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package sportify_backend.entite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportifyEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
